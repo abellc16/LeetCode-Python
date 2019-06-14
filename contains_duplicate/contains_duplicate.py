09ba1843a6e407176572c7be8ec13af27719600c
Cam <abellc16@students.ecu.edu>
2019-06-14 17:48:36 -0400

    Solution to contains_duplicates
    Analytics:
    Runtime: 120 ms, faster than 33.62% of Python online submissions for Contains Duplicate.
    Memory Usage: 16 MB, less than 92.06% of Python online submissions for Contains Duplicate.

---
+++ contains_duplicate/contains_duplicate.py
@@ -0,0 +1,29 @@
+# Author: Camby Abell
+# Filename: morse_code.py
+# URL: https://leetcode.com/problems/contains-duplicate/
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        if len(nums) == 1:
+            return False
+        else:
+            sorted_nums = sorted(nums)
+
+            for i in range(0, len(sorted_nums) - 1):
+                num1 = sorted_nums[i]
+                num2 = sorted_nums[i + 1]
+
+                if num1 == num2:
+                    return True
+
+        return False
+
+if __name__ == "__main__":
+    print(Solution.containsDuplicate(Solution, [1, 3, 2, 1]))
+    print(Solution.containsDuplicate(Solution, [6, 3, 7, 9, 10, 10494]))
+    print(Solution.containsDuplicate(Solution, [0]))
